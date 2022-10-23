3589c07b38c9adc240e54f617f28c50e3f1f6131
Nate Klobe <nate.klobe@outlook.com>
2022-10-23 11:25:02 -0500

    Updated world model to have required fields

--- models/world.model.js
+++ models/world.model.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const worldSchema = new Schema({
-    world_name: String,
-    creator: mongoose.Types.ObjectId,
-    description: String
+    world_name: {
+        type: String,
+        required: true
+    },
+    creator: {
+       type: mongoose.Types.ObjectId,
+       required: true
+    }, 
+    description: {
+        type: String,
+        required: true
+    },
 
 }, { timestamps: true, })
 
